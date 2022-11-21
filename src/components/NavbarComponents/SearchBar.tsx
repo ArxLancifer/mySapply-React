97e084953fc34709a758d25b7d0d4e433c5ce050
ArxLancifer <a_christoforidis@hotmail.com>
2022-11-21 15:21:15 +0200

    add state logic on search

--- src/components/NavbarComponents/SearchBar.tsx
+++ src/components/NavbarComponents/SearchBar.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container} from "@mui/system";
 import { TextField} from '@mui/material';
 
 function SearchBar() {
+
+    const [searchInput, setSearchInput] = useState("");
+
+    function searchHandler(e:any){
+        setSearchInput(e.target.value)
+    }
+
+    function proceedEnterHandler(e:any){
+        e.key === 'Enter' && console.log("Search request triggered with value: ", searchInput)
+    }
+
   return (
     <div>
        <Container maxWidth="md" sx={{py:4}}>
@@ -12,7 +23,9 @@ function SearchBar() {
           placeholder="Βρές οτι ψάχνεις για το κατάστημα σου!"
           type="search"
           variant="standard"
-          style ={{width: '100%'}} 
+          style ={{width: '100%'}}
+          onKeyDown={proceedEnterHandler}
+          onChange={searchHandler}
         />
        </Container>
     </div>
