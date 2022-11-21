f84baeeed63909f8fc77f5d8dacd04c891baa235
ArxLancifer <a_christoforidis@hotmail.com>
2022-11-21 15:59:08 +0200

    add fetch logic to free beers api

--- src/components/NavbarComponents/SearchBar.tsx
+++ src/components/NavbarComponents/SearchBar.tsx
@@ -1,17 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container} from "@mui/system";
 import { TextField} from '@mui/material';
 
 function SearchBar() {
 
     const [searchInput, setSearchInput] = useState("");
+    const [searchedProducts, setSearchedProducts] = useState("")
+    async function fetchSearchProducts(){
+        console.log("Search request triggered with value: ", searchInput)
+        const fetchProductData = await fetch(`https://api.punkapi.com/v2/beers?beer_name=${searchInput}`);
+        const productData = await fetchProductData.json();
+        setSearchedProducts(productData);
+        console.log(productData)
+    }
+
+    useEffect(()=>{console.log("useEffect triggered")},
+    [searchedProducts])
+
 
     function searchHandler(e:any){
         setSearchInput(e.target.value)
     }
 
     function proceedEnterHandler(e:any){
-        e.key === 'Enter' && console.log("Search request triggered with value: ", searchInput)
+        e.key === 'Enter' && fetchSearchProducts()
+        
     }
 
   return (
