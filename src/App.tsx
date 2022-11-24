4c4becb5581321b6713879304c4db28eb78ba602
batalamast <batalamast@gmail.com>
2022-11-24 14:00:19 +0200

    update Login-Signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React, { Fragment } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, {Fragment} from 'react';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
-import Header from './components/NavbarComponents/Header';
-import Login from './components/Login-Signup/Login';
-import Signup from './components/Login-Signup/Signup';
+import Header from "./components/NavbarComponents/Header";
+import Login from "./pages/Login-Signup/Login";
+import Signup from "./pages/Login-Signup/Signup";
 
 function App() {
-  return (
-    <Fragment>
-        <Header />
-    <Routes>
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<Signup/>} />
-    </Routes>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Header/>
+            <Routes>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/signup' element={<Signup/>}/>
+            </Routes>
+        </Fragment>
+    );
 }
 
 export default App;
