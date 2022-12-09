271745048ec08d4f0be454395627892c10d3e3b1
batalamast <batalamast@gmail.com>
2022-12-09 19:57:45 +0200

    update login page

--- src/pages/Login-Signup/Login.tsx
+++ src/pages/Login-Signup/Login.tsx
@@ -5,50 +5,51 @@ import LockOpenIcon from '@mui/icons-material/LockOpen';
 import {Container} from '@mui/system';
 import {Link, useNavigate} from 'react-router-dom';
 import LoginWithOneTap from "../../components/Login-Signup/LoginWithOneTap";
-import {  useEffect, useState } from 'react';
-
-
-
+import {useState} from 'react';
 
 function Login() {
 
-  
+    interface IModel {
+        _id: string;
+        updatedAt: Date | any;
+        createdAt: Date | any;
+    }
+
     interface IUserPost {
         username: string;
         password: string;
     }
-        
 
-    const navigate = useNavigate()
-    const [userInput, setUserInput] = useState<IUserPost>({username:"", password:""})
-    const [user , setUser] = useState<{username:string}>({username:"Not user yet"})
-    
+    interface IUser extends IModel{
+        email: string;
+        username: string;
+        confirmedUsers: any[];
+    }
 
-    function handleInput(e:any){
+    const navigate = useNavigate();
+    const [userInput, setUserInput] = useState<IUserPost>({username: "", password: ""});
+    const [user, setUser] = useState<{username: string}>({username: "Not user yet"});
+
+    function handleInput(e: any) {
         const inputField = e.target.id
         setUserInput({
             ...userInput,
-           [inputField]:e.target.value
-        })
+            [inputField]: e.target.value
+        });
     }
-    
 
-    async function logMeIn(e?:any){
-       
+    async function logMeIn(e?: any) {
         e.preventDefault()
         const reqLoginData: IUserPost = userInput;
-        const userAuthData = await fetch("http://localhost:5500/login",{
-            method:"POST",
-            headers:{'Content-Type': 'application/json'},
-            body:JSON.stringify(reqLoginData),
+        const userAuthData = await fetch("http://localhost:5500/login", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(reqLoginData),
             credentials: 'include'
-        })
-        
-        const userData = await userAuthData.json();
-        console.log(userData)
-       setUser(userData.username)
-        
-        
+        });
+
+        const userData: Pick<IUser, "username"> = await userAuthData.json();
+        setUser({username: userData.username});
     }
 
     // useEffect(()=>{
@@ -56,31 +57,32 @@ function Login() {
     //         navigate("/")
     //     }
     // }, [user])
-    
 
     return (
         <Box sx={{
             position: "absolute",
             top: "50%",
             left: "50%",
-            p:4,
+            p: 4,
             transform: "translate(-50%, -50%)",
             width: "60vw",
-            height:"50vh",
-            borderRadius:1 , 
+            height: "50vh",
+            borderRadius: 1,
             boxShadow: 3
         }}>
             <Container maxWidth="xs">
                 <form>
                     <FormControl fullWidth>
-                        <TextField sx={{my: 1}} id="username" label="Username" variant="filled" type="email" onChange={handleInput} InputProps={{
+                        <TextField sx={{my: 1}} id="username" label="Username" variant="filled" type="email"
+                                   onChange={handleInput} InputProps={{
                             endAdornment: (
                                 <InputAdornment position="start">
                                     <MailOutlineIcon/>
                                 </InputAdornment>
                             ),
                         }}/>
-                        <TextField sx={{my: 1}} id="password" label="Password" variant="filled" type="password" onChange={handleInput}
+                        <TextField sx={{my: 1}} id="password" label="Password" variant="filled" type="password"
+                                   onChange={handleInput}
                                    InputProps={{
                                        endAdornment: (
                                            <InputAdornment position="start">
@@ -110,7 +112,7 @@ function Login() {
                 </Box>
                 <LoginWithOneTap/>
             </Container>
-            <p>{JSON.stringify(user)}</p>
+            <p>{user.username}</p>
         </Box>
     )
 }
