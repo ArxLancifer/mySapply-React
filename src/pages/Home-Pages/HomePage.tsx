f0b11b29ab06dba328a5c2f1d4067ad27e7ec7cf
batalamast <batalamast@gmail.com>
2022-12-13 10:36:48 +0200

    update header with Login-Signup

--- src/pages/Home-Pages/HomePage.tsx
+++ src/pages/Home-Pages/HomePage.tsx
@@ -1,25 +1,25 @@
-import { Container } from '@mui/material'
+import {Container} from '@mui/material'
 import React from 'react'
 import CardsContainer from '../../components/CardsCollection/CardsContainer'
 import CategoryCard from '../../components/CardsCollection/CategoryCard'
 import DealsSlider from './Main/DealsSlider'
 
 function HomePage() {
-  return (
-    <Container>
-    <header>
-        <h1>Afou eixa</h1>
-    </header>
-    <DealsSlider />
-    <CardsContainer heading={"Κατηγορίες προϊόντων"}>
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-    </CardsContainer>
-    </Container>
-  )
+    return (
+        <Container>
+            <header>
+                <h1>Afou eixa</h1>
+            </header>
+            <DealsSlider/>
+            <CardsContainer heading={"Κατηγορίες προϊόντων"}>
+                <CategoryCard/>
+                <CategoryCard/>
+                <CategoryCard/>
+                <CategoryCard/>
+                <CategoryCard/>
+            </CardsContainer>
+        </Container>
+    )
 }
 
 export default HomePage
