02f2b17dddf84d0ed883ec133a0a73bc02b77e15
batalamast <batalamast@gmail.com>
2023-01-04 17:00:11 +0200

    add auto complete component to search product

--- src/pages/Product/components/SearchProduct.tsx
+++ src/pages/Product/components/SearchProduct.tsx
@@ -1,8 +1,10 @@
-import {Container, FormControl, TextField} from "@mui/material";
-import {ChangeEvent} from "react";
+import {Autocomplete, Container, FormControl, TextField} from "@mui/material";
+import {ChangeEvent, useEffect, useState} from "react";
 import axios from "axios";
+import {IProduct} from "../../../interfaces/IAlcoholDrink";
 
 function SearchProduct() {
+    const [options, setOptions] = useState<Partial<IProduct>[]>([]);
     const getProductByValue = async (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         const value = event.target.value;
         if (value.length > 2) {
@@ -11,25 +13,49 @@ function SearchProduct() {
         }
     }
 
+    const getProducts = async () => {
+        const data = {};
+        const response = await axios.post("http://localhost:5500/products", {data});
+        setOptions(response.data);
+    };
+
+    // useEffect(() => {
+    //     getProducts();
+    // }, []);
+
     return (
         <form>
             <Container maxWidth="sm">
-                <FormControl sx={{my: 4, width: "100%"}}>
-                    <TextField
-                        size="small"
-                        sx={{
-                            width: "100%",
-                            fieldset: {
-                                border: "1px solid #ccc",
-                                borderRadius: "50px",
-                            },
-                        }}
-                        type="search"
-                        id="search"
-                        placeholder="Γράψε τον όρο αναζήτησης"
-                        onChange={getProductByValue}
-                    />
-                </FormControl>
+                {/*<FormControl sx={{my: 4, width: "100%"}}>*/}
+                {/*    <TextField*/}
+                {/*        size="small"*/}
+                {/*        sx={{*/}
+                {/*            width: "100%",*/}
+                {/*            fieldset: {*/}
+                {/*                border: "1px solid #ccc",*/}
+                {/*                borderRadius: "50px",*/}
+                {/*            },*/}
+                {/*        }}*/}
+                {/*        type="search"*/}
+                {/*        id="search"*/}
+                {/*        placeholder="Γράψε τον όρο αναζήτησης"*/}
+                {/*        onChange={getProductByValue}*/}
+                {/*    />*/}
+                {/*</FormControl>*/}
+                <Autocomplete
+                    size="small"
+                    sx={{
+                        width: "100%",
+                        fieldset: {
+                            border: "1px solid #ccc",
+                            borderRadius: "50px",
+                        },
+                    }}
+                    disablePortal
+                    id="combo-box-demo"
+                    options={options}
+                    renderInput={(params) => <TextField {...params} label="Γράψε τον όρο αναζήτησης" />}
+                />
             </Container>
 
         </form>
