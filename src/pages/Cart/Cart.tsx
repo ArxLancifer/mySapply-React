5e03aafdd66ec9410e69c5edd849a2dc3c9c48d1
batalamast <batalamast@gmail.com>
2023-01-09 13:33:22 +0200

    add items to cart (test version)

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,43 @@
-import {Container, Typography} from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+import {IProduct} from "../../interfaces/IAlcoholDrink";
 
 function Cart() {
+    const [cartItems, setCartItems] = useState<IProduct[] | null>(null);
+    const getCartItems = () => {
+        const cartItem = localStorage.getItem("myCart")
+        if (cartItem) {
+            setCartItems(JSON.parse(cartItem));
+        }
+    }
+
+    useEffect(() => {
+        getCartItems()
+    }, []);
     return (
-        <Container>
+        <Container maxWidth="sm">
             <Typography sx={{fontWeight: "bold", mt: 3}} variant="h5">
                 Το καλάθι σου
             </Typography>
+            {cartItems?.length
+                ?
+                cartItems.map(cartItem => (
+                    <Box sx={{backgroundColor: "white", borderRadius: 1, boxShadow: 2, mt: 4}}>
+                        <Box sx={{display: "flex", justifyContent: "space-between", p: 2}}>
+                            <Typography>
+                                {cartItem?.title}
+                            </Typography>
+                            <Typography>
+                                {cartItem?.price}
+                            </Typography>
+                        </Box>
+                    </Box>
+                ))
+                :
+                <Typography sx={{mt: 3}} variant="body2">
+                    Το καλάθι είναι άδειο
+                </Typography>
+            }
         </Container>
     )
 }
--- src/pages/Product/components/AddToCart.tsx
+++ src/pages/Product/components/AddToCart.tsx
@@ -1,10 +1,58 @@
 import {Box, Button, Container, Typography} from "@mui/material";
 import {IProduct} from "../../../interfaces/IAlcoholDrink";
+import {useState} from "react";
 
 type Props = {
     product: Partial<IProduct>
 }
+
 function AddToCart({product}: Props) {
+    // const [itemsList, setItemsList] = useState<IProduct[] | null>(null);
+    let itemsList: IProduct[] = [];
+    const addToCart = (product: Partial<IProduct>) => {
+        // return localStorage.removeItem("myCart");
+
+        let quantity = 1;
+        let obj = {};
+        const localStorageCart = localStorage.getItem('myCart');
+        if (localStorageCart) {
+            const myCart: IProduct[] = JSON.parse(localStorageCart);
+            if (myCart.length) {
+                let productFound = myCart.find(c => c._id === product._id);
+                const cartItem = localStorage.getItem('myCart');
+                if (!productFound) {
+                    if (cartItem) {
+                        itemsList = JSON.parse(cartItem);
+                    }
+                    itemsList.push(product as IProduct);
+                    localStorage.setItem('myCart', JSON.stringify(itemsList));
+                } else {
+                    let arrUpdated: IProduct[] = [];
+                    productFound = {
+                        ...productFound,
+                        // @ts-ignore
+                        quantity: productFound.quantity ? productFound.quantity + 1 : quantity + 1
+                    };
+                    if (cartItem) {
+                        arrUpdated = JSON.parse(cartItem);
+                    }
+                    itemsList = arrUpdated.filter(c => c._id !== productFound?._id);
+                    localStorage.removeItem("myCart");
+                    if (productFound) {
+                        itemsList.push(productFound);
+                    }
+                    localStorage.setItem('myCart', JSON.stringify(itemsList));
+                }
+            }
+        } else {
+            itemsList.push(product as IProduct);
+            localStorage.setItem('myCart', JSON.stringify(itemsList));
+        }
+
+        // console.log("itemsList", itemsList);
+        // console.log("obj", obj);
+    };
+
     return (
         <Box sx={{
             display: {sm: "block", lg: "none"},
@@ -21,7 +69,8 @@ function AddToCart({product}: Props) {
                     {product?.price}
                 </Typography>
                 <Box>
-                    <Button sx={{px: 5, backgroundColor: "#43993e"}} color={"success"} variant="contained">
+                    <Button onClick={() => addToCart(product)} sx={{px: 5, backgroundColor: "#43993e"}}
+                            color={"success"} variant="contained">
                         Προσθήκη στο καλάθι
                     </Button>
                 </Box>
