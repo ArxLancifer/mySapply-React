e30ee03f418ccda872768279ddd1e18a875d9fb8
batalamast <batalamast@gmail.com>
2023-01-23 18:25:48 +0200

    update profile css

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,7 +4,9 @@ import {Box, Button, Container, Typography} from "@mui/material";
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import BuildIcon from '@mui/icons-material/Build';
+import CategoryIcon from '@mui/icons-material/Category';
 import {Link} from "react-router-dom";
+import styles from "./Profile.module.css";
 
 function Profile() {
     const {user, logout} = useContext(AuthContext);
@@ -19,9 +21,8 @@ function Profile() {
             <Typography sx={{mt: 4}} variant="h5">
                 Γεια σου {user.username}
             </Typography>
-            <Link to={"/profile/myorders"}>
-            <Box
-                sx={{backgroundColor: "#e3e3e3", display: "flex", alignItems: "center", py: 1, mt: 3, borderRadius: 2}}>
+            <Link style={linkStyle} to={"/profile/myorders"}>
+            <Box className={styles.tabs}>
                 <ReceiptIcon sx={{ml: 2, mr: 1}}/>
                 <Typography variant="h6">
                     Παραγγελίες
@@ -31,17 +32,20 @@ function Profile() {
             </Box>
             </Link>
             <Link style={linkStyle} to="/profile/settings">
-                <Box sx={{
-                    backgroundColor: "#e3e3e3",
-                    display: "flex",
-                    alignItems: "center",
-                    py: 1,
-                    mt: 3,
-                    borderRadius: 2
-                }}>
+                <Box className={styles.tabs}>
                     <BuildIcon sx={{ml: 2, mr: 1}}/>
                     <Typography variant="h6">
                         Ρυθμίσεις
+                    </Typography>
+                    <Box sx={{flexGrow: 1}}></Box>
+                    <ArrowForwardIosIcon sx={{mr: 1}}/>
+                </Box>
+            </Link>
+            <Link style={linkStyle} to="/admin/products/sub-categories">
+                <Box className={styles.tabs}>
+                    <CategoryIcon sx={{ml: 2, mr: 1}}/>
+                    <Typography variant="h6">
+                        Υποκατηγορίες
                     </Typography>
                     <Box sx={{flexGrow: 1}}></Box>
                     <ArrowForwardIosIcon sx={{mr: 1}}/>
