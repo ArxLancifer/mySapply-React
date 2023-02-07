ae050a450b584715b2c30e07ea608e6471aa4cd0
ArxLancifer <a_christoforidis@hotmail.com>
2023-02-07 20:52:29 +0200

    products layout

--- src/components/admin/Products/components/FilterProducts.tsx
+++ src/components/admin/Products/components/FilterProducts.tsx
@@ -1,6 +1,6 @@
-import { Box, Checkbox, Divider, FormControlLabel, FormGroup, InputAdornment, OutlinedInput, Slider, TextField, Typography } from '@mui/material'
+import { Box, Checkbox, Divider, FormControlLabel, FormGroup, InputAdornment, Slider, TextField, Typography } from '@mui/material'
 import EuroSymbolIcon from '@mui/icons-material/EuroSymbol';
-import React from 'react'
+import React, { useState } from 'react'
 
 
   
@@ -8,8 +8,23 @@ import React from 'react'
 
 function FilterProducts() {
 
-    const [values, setValues] = React.useState<number[]>([0, 100]);
+    const minProductPrice = 15;
+    const maxProductPrice = 150;
 
+    const [values, setValues] = React.useState<number[]>([minProductPrice, maxProductPrice]);
+
+    const [filterOptions, setFilterOptions] = useState({});
+
+    function checkBoxHandler(event:any){
+        setFilterOptions((prevOptions)=>{
+            return {
+                ...prevOptions,
+                [event.target.id]:event.target.checked
+            }
+        })
+    }
+    
+    console.log(filterOptions, "WTF");
     function handleSliderChange(event:any):[] | void{
         if(Math.abs(event.target.value[0]-event.target.value[1]) <= minDistance){
             return;
@@ -18,20 +33,20 @@ function FilterProducts() {
         setValues(event.target.value)
     }
 
-  
+    
 
     return (
 <div>
     <Typography variant='h5'>Φίλτρα</Typography>
-    <FormControlLabel control={<Checkbox size='small' />} label="Προσφορές" />
+    <FormControlLabel control={<Checkbox id='offers' size='small' onChange={checkBoxHandler} />} label="Προσφορές" />
     <Box sx={{mt:4}}>
         <Typography variant='subtitle1'>Εύρος τιμών</Typography>
         <TextField
-        sx={{width:"45%", height:"2px", mr:"1rem"}}
+        sx={{width:{xs:"80%", lg:"40%"}, display:{xs:"block", lg:"inline-block",}, m:1}}
         size='small'
             id="outlined-adornment-weight"
             InputProps={{
-                endAdornment: <InputAdornment position="end"><EuroSymbolIcon /></InputAdornment>,
+                endAdornment: <InputAdornment position="end"><EuroSymbolIcon sx={{fontSize:"1rem"}}/></InputAdornment>,
               }}
             aria-describedby="outlined-weight-helper-text"
             inputProps={{
@@ -40,11 +55,11 @@ function FilterProducts() {
             value={values[0]}
           />
         <TextField
-        sx={{width:"45%"}}
+        sx={{width:{xs:"80%", lg:"40%"}, display:{xs:"block", lg:"inline-block" }, m:1}}
         size='small'
             id="outlined-adornment-weight"
             InputProps={{
-                endAdornment: <InputAdornment position="end"><EuroSymbolIcon /></InputAdornment>,
+                endAdornment: <InputAdornment position="end"><EuroSymbolIcon sx={{fontSize:"1rem"}}/></InputAdornment>,
               }}
             aria-describedby="outlined-weight-helper-text"
             inputProps={{
@@ -63,17 +78,17 @@ function FilterProducts() {
         <Divider sx={{my:4}}/>
         <Typography variant='subtitle1'>Είδος</Typography>
         <FormGroup>
-        <FormControlLabel control={<Checkbox defaultChecked size='small' />} label="Βοτκα" />
-        <FormControlLabel control={<Checkbox size='small' />} label="Τζιν" />
-        <FormControlLabel control={<Checkbox size='small' />} label="Ουίσκι" />
+        <FormControlLabel control={<Checkbox id='vodka' onChange={checkBoxHandler} size='small' />} label="Βοτκα" />
+        <FormControlLabel control={<Checkbox id='gin' onChange={checkBoxHandler} size='small' />} label="Τζιν" />
+        <FormControlLabel control={<Checkbox id='whiskey' onChange={checkBoxHandler} size='small' />} label="Ουίσκι" />
         </FormGroup>
         <Divider sx={{my:4}}/>
         <Typography variant='subtitle1'>Brands</Typography>
         {/* Fetch and map all branch from database */}
         <FormGroup>
-        <FormControlLabel control={<Checkbox size='small' />} label="Absolute" />
-        <FormControlLabel control={<Checkbox size='small' />} label="Belveder" />
-        <FormControlLabel control={<Checkbox size='small' />} label="Smirnoff" />
+        <FormControlLabel control={<Checkbox id='absolute' onChange={checkBoxHandler} size='small' />} label="Absolute" />
+        <FormControlLabel control={<Checkbox id='belveder' onChange={checkBoxHandler} size='small' />} label="Belveder" />
+        <FormControlLabel control={<Checkbox id='smirnoff' onChange={checkBoxHandler} size='small' />} label="Smirnoff" />
         </FormGroup>
             </Box>
 </div>
