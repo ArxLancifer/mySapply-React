04ebe122dac1f5fd213eb747847100affe1c2a14
batalamast <batalamast@gmail.com>
2023-03-06 21:45:19 +0200

    adds favorites method

--- src/pages/ProductSubCategories/components/AlcoholDrinks.tsx
+++ src/pages/ProductSubCategories/components/AlcoholDrinks.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
 import {IAlcoholDrink} from "../../../interfaces/IAlcoholDrink";
 import {Box, Container, Typography} from "@mui/material";
 import {IProductCategory, IProductSubCategory} from "../../../interfaces/ICategory";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import axios from "axios";
+import AuthContext from "../../../components/store/auth/AuthContext";
 
 type Props = {
     alcoholDrink: IAlcoholDrink;
@@ -11,60 +13,68 @@ type Props = {
 }
 
 function AlcoholDrinks({alcoholDrink, productSubCategory}: Props) {
+    const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onFavoriteIcon = async () => {
+        await axios.post(`http://localhost:5500/favorites`, {product: alcoholDrink, userId: user._id});
+    };
 
     return (
-        <Link
-            className="link-style"
-            to={`/products/${(productSubCategory?.category as IProductCategory)?.slug}/${productSubCategory?.slug}/${alcoholDrink?.slug}`}
-        >
-            <Box sx={{
-                borderRadius: "8px",
-                backgroundColor: "white",
-                boxShadow: "0px 0px 10px -1px rgba(219,219,219,0.6)",
-                display: "flex",
-                flexDirection: {xs: "row", lg: "column"},
-                justifyContent: "space-around",
-                alignItems: {xs: "center", lg: "baseline"},
-                width: "100%",
-                border: "1px solid #e8e8e8",
-                py: 2
-            }}>
-                <Container>
-                    <Box sx={{display: "flex", justifyContent: "end"}}>
-                        <FavoriteBorderIcon sx={{color: "#707070"}} />
-                    </Box>
+        <Box sx={{
+            borderRadius: "8px",
+            backgroundColor: "white",
+            boxShadow: "0px 0px 10px -1px rgba(219,219,219,0.6)",
+            display: "flex",
+            flexDirection: {xs: "row", lg: "column"},
+            justifyContent: "space-around",
+            alignItems: {xs: "center", lg: "baseline"},
+            width: "100%",
+            border: "1px solid #e8e8e8",
+            py: 2
+        }}>
+            <Container>
+                <Box sx={{display: "flex", justifyContent: "end"}}>
+                    <FavoriteBorderIcon sx={{color: "#707070", cursor: "pointer"}} onClick={onFavoriteIcon}/>
+                </Box>
+                <Link
+                    className="link-style"
+                    to={`/products/${(productSubCategory?.category as IProductCategory)?.slug}/${productSubCategory?.slug}/${alcoholDrink?.slug}`}
+                >
                     <Box sx={{width: "160px", mx: "auto", my: 2}}>
                         <img style={{width: "100%"}} src="https://www.thecocktaildb.com/images/ingredients/vodka.png"
                              alt=""/>
                     </Box>
-                    <Box sx={{width: "100%", flexGrow: 1}}>
-                        <Typography sx={{color: "#363636", fontWeight: 400, lineHeight: "19px", fontSize: "0.8rem"}} variant="subtitle1">
-                            {alcoholDrink.brandName}
-                        </Typography>
-                        <Box>
-                            {/*<Typography variant="h5" component="h5">{alcoholDrink.brandName}</Typography>*/}
-                            <Typography sx={{height: "30px", overflow: "hidden", textOverflow: "ellipsis", fontSize: "1.2rem"}}>
-                                {alcoholDrink.title}
-                            </Typography>
-                        </Box>
-                        <Typography sx={{color: "#707070", fontSize: "14px", lineHeight: "18px"}}>
-                            Βάρος: {alcoholDrink.weightML || 0} mL, Vol : {alcoholDrink.alcoholVol || 0} %
-                        </Typography>
-                    </Box>
+                </Link>
+                <Box sx={{width: "100%", flexGrow: 1}}>
+                    <Typography sx={{color: "#363636", fontWeight: 400, lineHeight: "19px", fontSize: "0.8rem"}}
+                                variant="subtitle1">
+                        {alcoholDrink.brandName}
+                    </Typography>
                     <Box>
-                        <Typography sx={{mt: 2, fontWeight: "bold", fontSize: "1.4rem"}} variant="h6">
-                            {alcoholDrink.price || 0} €
+                        {/*<Typography variant="h5" component="h5">{alcoholDrink.brandName}</Typography>*/}
+                        <Typography
+                            sx={{height: "30px", overflow: "hidden", textOverflow: "ellipsis", fontSize: "1.2rem"}}>
+                            {alcoholDrink.title}
                         </Typography>
                     </Box>
-                    <Typography sx={{mt: 1, color: "#707070", fontSize: "0.8rem"}}>
-                        στο mySapply
+                    <Typography sx={{color: "#707070", fontSize: "14px", lineHeight: "18px"}}>
+                        Βάρος: {alcoholDrink.weightML || 0} mL, Vol : {alcoholDrink.alcoholVol || 0} %
                     </Typography>
-                    <Typography sx={{color: "#295f27", fontSize: "0.8rem"}}>
-                        με express παράδοση
+                </Box>
+                <Box>
+                    <Typography sx={{mt: 2, fontWeight: "bold", fontSize: "1.4rem"}} variant="h6">
+                        {alcoholDrink.price || 0} €
                     </Typography>
-                </Container>
-            </Box>
-        </Link>
+                </Box>
+                <Typography sx={{mt: 1, color: "#707070", fontSize: "0.8rem"}}>
+                    στο mySapply
+                </Typography>
+                <Typography sx={{color: "#295f27", fontSize: "0.8rem"}}>
+                    με express παράδοση
+                </Typography>
+            </Container>
+        </Box>
     )
 }
 
